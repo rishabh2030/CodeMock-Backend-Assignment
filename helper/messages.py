3a5763c75a3d27ffa108fa5eb943a1d034612915
rishabh2030 <rishabh.sharma1@ssipmt.com>
2024-02-01 21:34:45 +0530

    Add messages.py to helper folder with predefined messages
    - Create messages.py in the helper folder
    - Include predefined messages for various scenarios:
      - PROVIDE_FIELDS: Please provide all fields
      - INCORRECT_DATA: Submitted Data is incorrect
      - REGISTRATION_USER: Registration Successful
      - LOGIN_SUCCESS: Login Successful
      - OPERATION_SUCCESS: Operation Successful
      - OPERATION_FAILED: Operation Failed
      - NO_DATA: No related Data is there!
      - POST_DELETED: Post Deleted
    - Organize messages to enhance code readability and maintainability
    
    This commit introduces a messages.py file in the helper folder, containing predefined messages for common scenarios. The messages are organized for better code structure and can be easily imported and used throughout the project to ensure consistent and clear messaging.

---
+++ helper/messages.py
@@ -0,0 +1,8 @@
+PROVIDE_FIELDS = "Please provide all fields"
+INCORRECT_DATA = "Submitted Data is incorrect"
+REGISTRACTION_USER = 'Registration Successful'
+LOGIN_SUCCESS = "Login Successful" 
+OPERATION_SUCCESS = "Operation Successful"
+OPERATION_FAILED = "Operation Failed"
+NO_DATA = "No related Data is there!"
+POST_DELETED = "Post Deleted"
